9decaa0b09918042ccb20ba1a6285cf7a6615e1f
Milad Miladi <miladim@cs.uni-freiburg.de>
2020-04-11 19:00:07 +0200

    missing update wrapoer

--- lib/RNA_SNV_wrapper/snv_wrapper.py
+++ lib/RNA_SNV_wrapper/snv_wrapper.py
@@ -136,7 +136,7 @@ def run_remuRNA(wild_fa, snp_tags, window=None):
         Pandas table of standard output
 
     """
-    assert(len(snp_tags) == 1)
+    # assert(len(snp_tags) == 1)
     if not os.path.isfile(wild_fa):
         raise RuntimeError("Input fasta %s does not exist" % in_fasta_file)
 
@@ -155,12 +155,12 @@ def run_remuRNA(wild_fa, snp_tags, window=None):
     if window is not None:
         params += '-w={}'.format(int(window))
     cmd += params
-    # print cmd
+    print (cmd)
     p = Popen(cmd , stdin=PIPE, shell=True, stdout=PIPE, stderr=PIPE)
     out, err = p.communicate()
     if err:
         print("Error in calling remuRNA\n{}\n{}\n".format(out, err))
-        df = pd.DataFrame({'SNP':{0:snp_tags[0]}, 'error':{0:'True:{}'.format(err.decode('utf-8'))}, })
+        df = pd.DataFrame({'SNP':{0:snp_tag[0]}, 'error':{0:'True:{}'.format(err.decode('utf-8'))}, })
     else:
         df = pd.read_table(StringIO(out.decode("utf-8")))
     df['remurna_params'] = params
